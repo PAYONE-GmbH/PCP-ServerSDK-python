dc4b3e71f704ecef49a9502c0bfac6642b037a0b
Blackfaded <r.heinen@nanogiants.de>
2024-08-19 09:07:13 +0200

    refactor: export models

--- pcp_serversdk_python/models/__init__.py
+++ pcp_serversdk_python/models/__init__.py
@@ -1,230 +1,230 @@
-# from .APIError import *
-# from .Address import *
-# from .AddressPersonal import *
-# from .AllowedPaymentActions import *
-# from .AmountOfMoney import *
-# from .ApplePaymentDataTokenHeaderInformation import *
-# from .ApplePaymentDataTokenInformation import *
-# from .ApplePaymentTokenVersion import *
-# from .AppliedExemption import *
-# from .AuthorizationMode import *
-# from .BankAccountInformation import *
-# from .CancelItem import *
-# from .CancelPaymentRequest import *
-# from .CancelPaymentResponse import *
-# from .CancelRequest import *
-# from .CancelResponse import *
-# from .CancelType import *
-# from .CartItemInput import *
-# from .CartItemInvoiceData import *
-# from .CartItemOrderStatus import *
-# from .CartItemPatch import *
-# from .CartItemResult import *
-# from .CartItemStatus import *
-# from .CheckoutReferences import *
-# from .CheckoutResponse import *
+from .APIError import *
+from .Address import *
+from .AddressPersonal import *
+from .AllowedPaymentActions import *
+from .AmountOfMoney import *
+from .ApplePaymentDataTokenHeaderInformation import *
+from .ApplePaymentDataTokenInformation import *
+from .ApplePaymentTokenVersion import *
+from .AppliedExemption import *
+from .AuthorizationMode import *
+from .BankAccountInformation import *
+from .CancelItem import *
+from .CancelPaymentRequest import *
+from .CancelPaymentResponse import *
+from .CancelRequest import *
+from .CancelResponse import *
+from .CancelType import *
+from .CartItemInput import *
+from .CartItemInvoiceData import *
+from .CartItemOrderStatus import *
+from .CartItemPatch import *
+from .CartItemResult import *
+from .CartItemStatus import *
+from .CheckoutReferences import *
+from .CheckoutResponse import *
 from .CheckoutsResponse import *
-# from .CommerceCaseResponse import *
-# from .CompanyInformation import *
-# from .CompleteFinancingPaymentMethodSpecificInput import *
-# from .CompletePaymentMethodSpecificInput import *
-# from .CompletePaymentRequest import *
-# from .CompletePaymentResponse import *
-# from .ContactDetails import *
-# from .CreateCheckoutRequest import *
-# from .CreateCheckoutResponse import *
-# from .CreateCommerceCaseRequest import *
-# from .CreateCommerceCaseResponse import *
-# from .CreatePaymentResponse import *
-# from .Customer import *
-# from .CustomerDevice import *
-# from .DeliverItem import *
-# from .DeliverRequest import *
-# from .DeliverResponse import *
-# from .DeliverType import *
-# from .DeliveryInformation import *
-# from .ErrorResponse import *
-# from .ExtendedCheckoutStatus import *
-# from .FinancingPaymentMethodSpecificInput import *
-# from .FinancingPaymentMethodSpecificOutput import *
-# from .Gender import *
-# from .InstallmentOption import *
-# from .LinkInformation import *
-# from .MandateRecurrenceType import *
-# from .MerchantAction import *
-# from .MobilePaymentMethodSpecificInput import *
-# from .MobilePaymentMethodSpecificOutput import *
-# from .Order import *
-# from .OrderItem import *
-# from .OrderLineDetailsInput import *
-# from .OrderLineDetailsPatch import *
-# from .OrderLineDetailsResult import *
-# from .OrderRequest import *
-# from .OrderResponse import *
-# from .OrderType import *
-# from .PayoutOutput import *
-# from .PayoutResponse import *
-# from .PaymentChannel import *
-# from .PaymentCreationOutput import *
-# from .PaymentExecutionSpecificInput import *
-# from .PaymentExecutionRequest import *
-# from .PaymentInformationRequest import *
-# from .PaymentInformationResponse import *
-# from .PaymentMethodSpecificInput import *
-# from .PaymentOutput import *
-# from .PaymentProduct320SpecificInput import *
-# from .PaymentProduct3391SpecificInput import *
-# from .PaymentProduct3391SpecificOutput import *
-# from .PaymentProduct3392SpecificInput import *
-# from .PaymentProduct771SpecificOutput import *
-# from .PaymentProduct840CustomerAccount import *
-# from .PaymentProduct840SpecificOutput import *
-# from .PaymentReferences import *
-# from .PaymentResponse import *
-# from .PaymentStatus import *
-# from .PaymentStatusOutput import *
-# from .PersonalInformation import *
-# from .PersonalName import *
-# from .PositiveAmountOfMoney import *
-# from .ProductType import *
-# from .ProcessingMandateInformation import *
-# from .RedirectionData import *
-# from .RedirectPaymentMethodSpecificInput import *
-# from .RedirectPaymentMethodSpecificOutput import *
-# from .RedirectPaymentProduct840SpecificInput import *
-# from .ReturnInformation import *
-# from .ReturnItem import *
-# from .ReturnRequest import *
-# from .ReturnResponse import *
-# from .ReturnType import *
-# from .SepaDirectDebitPaymentMethodSpecificInput import *
-# from .SepaDirectDebitPaymentMethodSpecificOutput import *
-# from .SepaDirectDebitPaymentProduct771SpecificInput import *
-# from .Shipping import *
-# from .ShoppingCartInput import *
-# from .ShoppingCartPatch import *
-# from .ShoppingCartResult import *
-# from .StatusCategoryValue import *
-# from .StatusCheckout import *
-# from .StatusOutput import *
-# from .StatusValue import *
-# from .ThreeDSecureResults import *
-# from .TransactionChannel import *
-# from .UnscheduledCardOnFileRequestor import *
-# from .UnscheduledCardOnFileSequenceIndicator import *
+from .CommerceCaseResponse import *
+from .CompanyInformation import *
+from .CompleteFinancingPaymentMethodSpecificInput import *
+from .CompletePaymentMethodSpecificInput import *
+from .CompletePaymentRequest import *
+from .CompletePaymentResponse import *
+from .ContactDetails import *
+from .CreateCheckoutRequest import *
+from .CreateCheckoutResponse import *
+from .CreateCommerceCaseRequest import *
+from .CreateCommerceCaseResponse import *
+from .CreatePaymentResponse import *
+from .Customer import *
+from .CustomerDevice import *
+from .DeliverItem import *
+from .DeliverRequest import *
+from .DeliverResponse import *
+from .DeliverType import *
+from .DeliveryInformation import *
+from .ErrorResponse import *
+from .ExtendedCheckoutStatus import *
+from .FinancingPaymentMethodSpecificInput import *
+from .FinancingPaymentMethodSpecificOutput import *
+from .Gender import *
+from .InstallmentOption import *
+from .LinkInformation import *
+from .MandateRecurrenceType import *
+from .MerchantAction import *
+from .MobilePaymentMethodSpecificInput import *
+from .MobilePaymentMethodSpecificOutput import *
+from .Order import *
+from .OrderItem import *
+from .OrderLineDetailsInput import *
+from .OrderLineDetailsPatch import *
+from .OrderLineDetailsResult import *
+from .OrderRequest import *
+from .OrderResponse import *
+from .OrderType import *
+from .PayoutOutput import *
+from .PayoutResponse import *
+from .PaymentChannel import *
+from .PaymentCreationOutput import *
+from .PaymentExecutionSpecificInput import *
+from .PaymentExecutionRequest import *
+from .PaymentInformationRequest import *
+from .PaymentInformationResponse import *
+from .PaymentMethodSpecificInput import *
+from .PaymentOutput import *
+from .PaymentProduct320SpecificInput import *
+from .PaymentProduct3391SpecificInput import *
+from .PaymentProduct3391SpecificOutput import *
+from .PaymentProduct3392SpecificInput import *
+from .PaymentProduct771SpecificOutput import *
+from .PaymentProduct840CustomerAccount import *
+from .PaymentProduct840SpecificOutput import *
+from .PaymentReferences import *
+from .PaymentResponse import *
+from .PaymentStatus import *
+from .PaymentStatusOutput import *
+from .PersonalInformation import *
+from .PersonalName import *
+from .PositiveAmountOfMoney import *
+from .ProductType import *
+from .ProcessingMandateInformation import *
+from .RedirectionData import *
+from .RedirectPaymentMethodSpecificInput import *
+from .RedirectPaymentMethodSpecificOutput import *
+from .RedirectPaymentProduct840SpecificInput import *
+from .ReturnInformation import *
+from .ReturnItem import *
+from .ReturnRequest import *
+from .ReturnResponse import *
+from .ReturnType import *
+from .SepaDirectDebitPaymentMethodSpecificInput import *
+from .SepaDirectDebitPaymentMethodSpecificOutput import *
+from .SepaDirectDebitPaymentProduct771SpecificInput import *
+from .Shipping import *
+from .ShoppingCartInput import *
+from .ShoppingCartPatch import *
+from .ShoppingCartResult import *
+from .StatusCategoryValue import *
+from .StatusCheckout import *
+from .StatusOutput import *
+from .StatusValue import *
+from .ThreeDSecureResults import *
+from .TransactionChannel import *
+from .UnscheduledCardOnFileRequestor import *
+from .UnscheduledCardOnFileSequenceIndicator import *
 
 __all__ = [
-    # 'APIError',
-    # 'Address',
-    # 'AddressPersonal',
-    # 'AllowedPaymentActions',
-    # 'AmountOfMoney',
-    # 'ApplePaymentDataTokenHeaderInformation',
-    # 'ApplePaymentDataTokenInformation',
-    # 'ApplePaymentTokenVersion',
-    # 'AppliedExemption',
-    # 'AuthorizationMode',
-    # 'BankAccountInformation',
-    # 'CancelItem',
-    # 'CancelPaymentRequest',
-    # 'CancelPaymentResponse',
-    # 'CancelRequest',
-    # 'CancelResponse',
-    # 'CancelType',
-    # 'CartItemInput',
-    # 'CartItemInvoiceData',
-    # 'CartItemOrderStatus',
-    # 'CartItemPatch',
-    # 'CartItemResult',
-    # 'CartItemStatus',
-    # 'CheckoutReferences',
-    # 'CheckoutResponse',
+    'APIError',
+    'Address',
+    'AddressPersonal',
+    'AllowedPaymentActions',
+    'AmountOfMoney',
+    'ApplePaymentDataTokenHeaderInformation',
+    'ApplePaymentDataTokenInformation',
+    'ApplePaymentTokenVersion',
+    'AppliedExemption',
+    'AuthorizationMode',
+    'BankAccountInformation',
+    'CancelItem',
+    'CancelPaymentRequest',
+    'CancelPaymentResponse',
+    'CancelRequest',
+    'CancelResponse',
+    'CancelType',
+    'CartItemInput',
+    'CartItemInvoiceData',
+    'CartItemOrderStatus',
+    'CartItemPatch',
+    'CartItemResult',
+    'CartItemStatus',
+    'CheckoutReferences',
+    'CheckoutResponse',
     'CheckoutsResponse',
-    # 'CommerceCaseResponse',
-    # 'CompanyInformation',
-    # 'CompleteFinancingPaymentMethodSpecificInput',
-    # 'CompletePaymentMethodSpecificInput',
-    # 'CompletePaymentRequest',
-    # 'CompletePaymentResponse',
-    # 'ContactDetails',
-    # 'CreateCheckoutRequest',
-    # 'CreateCheckoutResponse',
-    # 'CreateCommerceCaseRequest',
-    # 'CreateCommerceCaseResponse',
-    # 'CreatePaymentResponse',
-    # 'Customer',
-    # 'CustomerDevice',
-    # 'DeliverItem',
-    # 'DeliverRequest',
-    # 'DeliverResponse',
-    # 'DeliveryInformation',
-    # 'ErrorResponse',
-    # 'ExtendedCheckoutStatus',
-    # 'FinancingPaymentMethodSpecificInput',
-    # 'FinancingPaymentMethodSpecificOutput',
-    # 'Gender',
-    # 'InstallmentOption',
-    # 'LinkInformation',
-    # 'MandateRecurrenceType',
-    # 'MerchantAction',
-    # 'MobilePaymentMethodSpecificInput',
-    # 'MobilePaymentMethodSpecificOutput',
-    # 'Order',
-    # 'OrderItem',
-    # 'OrderLineDetailsInput',
-    # 'OrderLineDetailsPatch',
-    # 'OrderLineDetailsResult',
-    # 'OrderRequest',
-    # 'OrderResponse',
-    # 'OrderType',
-    # 'PayoutOutput',
-    # 'PayoutResponse',
-    # 'PaymentChannel',
-    # 'PaymentCreationOutput',
-    # 'PaymentExecution',
-    # 'PaymentExecutionRequest',
-    # 'PaymentExecutionSpecificInput',
-    # 'PaymentEvent',
-    # 'PaymentInformationRequest',
-    # 'PaymentInformationResponse',
-    # 'PaymentMethodSpecificInput',
-    # 'PaymentOutput',
-    # 'PaymentProduct320SpecificInput',
-    # 'PaymentProduct3391SpecificInput',
-    # 'PaymentProduct3391SpecificOutput',
-    # 'PaymentProduct3392SpecificInput',
-    # 'PaymentProduct771SpecificOutput',
-    # 'PaymentProduct840CustomerAccount',
-    # 'PaymentProduct840SpecificOutput',
-    # 'PaymentReferences',
-    # 'PaymentResponse',
-    # 'PaymentStatus',
-    # 'PaymentStatusOutput',
-    # 'PersonalInformation',
-    # 'PersonalName',
-    # 'PositiveAmountOfMoney',
-    # 'ProductType',
-    # 'RedirectData',
-    # 'RedirectPaymentMethodSpecificInput',
-    # 'RedirectPaymentMethodSpecificOutput',
-    # 'RedirectPaymentProduct840SpecificInput',
-    # 'RefundErrorResponse',
-    # 'RefundOutput',
-    # 'RefundPaymentResponse',
-    # 'RefundRequest',
-    # 'SepaDirectDebitPaymentMethodSpecificInput',
-    # 'SepaDirectDebitPaymentMethodSpecificOutput',
-    # 'SepaDirectDebitPaymentProduct771SpecificInput',
-    # 'Shipping',
-    # 'ShoppingCartInput',
-    # 'ShoppingCartPatch',
-    # 'ShoppingCartResult',
-    # 'StatusCategoryValue',
-    # 'StatusCheckout',
-    # 'StatusOutput',
-    # 'StatusValue',
-    # 'ThreeDSecureResults',
-    # 'TransactionChannel',
-    # 'UnscheduledCardOnFileRequestor',
-    # 'UnscheduledCardOnFileSequenceIndicator',
+    'CommerceCaseResponse',
+    'CompanyInformation',
+    'CompleteFinancingPaymentMethodSpecificInput',
+    'CompletePaymentMethodSpecificInput',
+    'CompletePaymentRequest',
+    'CompletePaymentResponse',
+    'ContactDetails',
+    'CreateCheckoutRequest',
+    'CreateCheckoutResponse',
+    'CreateCommerceCaseRequest',
+    'CreateCommerceCaseResponse',
+    'CreatePaymentResponse',
+    'Customer',
+    'CustomerDevice',
+    'DeliverItem',
+    'DeliverRequest',
+    'DeliverResponse',
+    'DeliveryInformation',
+    'ErrorResponse',
+    'ExtendedCheckoutStatus',
+    'FinancingPaymentMethodSpecificInput',
+    'FinancingPaymentMethodSpecificOutput',
+    'Gender',
+    'InstallmentOption',
+    'LinkInformation',
+    'MandateRecurrenceType',
+    'MerchantAction',
+    'MobilePaymentMethodSpecificInput',
+    'MobilePaymentMethodSpecificOutput',
+    'Order',
+    'OrderItem',
+    'OrderLineDetailsInput',
+    'OrderLineDetailsPatch',
+    'OrderLineDetailsResult',
+    'OrderRequest',
+    'OrderResponse',
+    'OrderType',
+    'PayoutOutput',
+    'PayoutResponse',
+    'PaymentChannel',
+    'PaymentCreationOutput',
+    'PaymentExecution',
+    'PaymentExecutionRequest',
+    'PaymentExecutionSpecificInput',
+    'PaymentEvent',
+    'PaymentInformationRequest',
+    'PaymentInformationResponse',
+    'PaymentMethodSpecificInput',
+    'PaymentOutput',
+    'PaymentProduct320SpecificInput',
+    'PaymentProduct3391SpecificInput',
+    'PaymentProduct3391SpecificOutput',
+    'PaymentProduct3392SpecificInput',
+    'PaymentProduct771SpecificOutput',
+    'PaymentProduct840CustomerAccount',
+    'PaymentProduct840SpecificOutput',
+    'PaymentReferences',
+    'PaymentResponse',
+    'PaymentStatus',
+    'PaymentStatusOutput',
+    'PersonalInformation',
+    'PersonalName',
+    'PositiveAmountOfMoney',
+    'ProductType',
+    'RedirectData',
+    'RedirectPaymentMethodSpecificInput',
+    'RedirectPaymentMethodSpecificOutput',
+    'RedirectPaymentProduct840SpecificInput',
+    'RefundErrorResponse',
+    'RefundOutput',
+    'RefundPaymentResponse',
+    'RefundRequest',
+    'SepaDirectDebitPaymentMethodSpecificInput',
+    'SepaDirectDebitPaymentMethodSpecificOutput',
+    'SepaDirectDebitPaymentProduct771SpecificInput',
+    'Shipping',
+    'ShoppingCartInput',
+    'ShoppingCartPatch',
+    'ShoppingCartResult',
+    'StatusCategoryValue',
+    'StatusCheckout',
+    'StatusOutput',
+    'StatusValue',
+    'ThreeDSecureResults',
+    'TransactionChannel',
+    'UnscheduledCardOnFileRequestor',
+    'UnscheduledCardOnFileSequenceIndicator',
 ]
