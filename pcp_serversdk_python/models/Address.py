11a177f8d57b049c07510c0c5300021d035f8a0a
Blackfaded <r.heinen@nanogiants.de>
2024-08-19 10:02:37 +0200

    chore: rename

--- pcp_serversdk_python/models/Address.py
+++ pcp_serversdk_python/models/Address.py
@@ -4,10 +4,10 @@ from dataclasses import dataclass
 
 @dataclass(kw_only=True)
 class Address:
-    additionalInfo: Optional[str] = (None,)
-    city: Optional[str] = (None,)
-    countryCode: Optional[str] = (None,)
-    houseNumber: Optional[str] = (None,)
-    state: Optional[str] = (None,)
-    street: Optional[str] = (None,)
+    additionalInfo: Optional[str] = None,
+    city: Optional[str] = None,
+    countryCode: Optional[str] = None,
+    houseNumber: Optional[str] = None,
+    state: Optional[str] = None,
+    street: Optional[str] = None,
     zip: Optional[str] = None
