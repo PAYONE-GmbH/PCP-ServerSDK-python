e87e874e319425fd3e3f7b5ebfcf9953cba6e564
Blackfaded <r.heinen@nanogiants.de>
2024-08-21 12:01:54 +0200

    chore: update scripts

--- scripts.sh
+++ scripts.sh
@@ -12,6 +12,7 @@ setup() {
 # Function to install dependencies
 install() {
     echo "Installing dependencies..."
+    npm install
     pcp_serversdk_python/bin/pip install -r requirements.txt
     echo "Dependencies installed."
 }
@@ -37,6 +38,34 @@ lint() {
     echo "Lint complete."
 }
 
+version() {
+    # example: ./scripts.sh version 1.0.0
+    if [ -z "$2" ]; then
+        echo "Please provide the version."
+        exit 1
+    fi
+    NEW_VERSION=$2
+    SETUP_PY_PATH='./setup.py'
+    SERVER_META_INFO_PATH='./pcp_serversdk_python/utils/ServerMetaInfo.py'
+    SERVER_META_INFO_TEST_PATH='./tests/utils/test_ServerMetaInfo.py'
+    echo "Getting the version..."
+    sed -i '' "s/version=\"[0-9]*\.[0-9]*\.[0-9]*\",/version=\"$NEW_VERSION\",/" ${SETUP_PY_PATH}
+    sed -i '' "s/PythonServerSDK\/v[0-9]*\.[0-9]*\.[0-9]*/PythonServerSDK\/v$NEW_VERSION/" ${SERVER_META_INFO_PATH}
+    sed -i '' "s/PythonServerSDK\/v[0-9]*\.[0-9]*\.[0-9]*/PythonServerSDK\/v$NEW_VERSION/" ${SERVER_META_INFO_TEST_PATH}
+
+    echo "Version complete."
+
+    git add $SETUP_PY_PATH
+    git add $SERVER_META_INFO_PATH
+    git add $SERVER_META_INFO_TEST_PATH
+    echo "Updated $SETUP_PY_PATH with version $NEW_VERSION"
+    echo "Updated $SERVER_META_INFO_PATH with version $NEW_VERSION"
+    echo "Updated $SERVER_META_INFO_TEST_PATH with version $NEW_VERSION"
+    npm run changelog
+    git add CHANGELOG.md
+    echo "Updated CHANGELOG.md"
+}
+
 clear() {
     echo "Removing __pycache__ directories..."
     find . -type d -name "__pycache__" -exec rm -rf {} +
@@ -57,15 +86,16 @@ clear() {
     echo "All __pycache__ directories have been removed."
 }
 
-# Function to publish the package
 publish() {
+    # example: ./scripts.sh publish <PyPI token>
     # check if the PyPI token is passed
     if [ -z "$2" ]; then
         echo "Please provide the PyPI token."
         exit 1
     fi
     echo "Uploading the package..."
-    pcp_serversdk_python/bin/twine upload dist/* -u __token__ -p $2
+    TOKEN=$2
+    pcp_serversdk_python/bin/twine upload dist/* -u __token__ -p $TOKEN
     echo "Upload complete."
 }
 
@@ -89,11 +119,14 @@ lint)
 clear)
     clear
     ;;
+version)
+    version $@
+    ;;
 publish)
-    publish
+    publish $@
     ;;
 *)
-    echo "Usage: $0 {setup|install|build|test|publish}"
+    echo "Usage: $0 {setup|install|build|test|lint|clear|version|publish}"
     exit 1
     ;;
 esac
