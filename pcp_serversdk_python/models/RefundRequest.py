91cf4d6f3a694e2916a93c36060a8636a0fa989f
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2024-08-22 11:17:11 +0200

    chore: add handling for "return" field in model

--- pcp_serversdk_python/models/RefundRequest.py
+++ pcp_serversdk_python/models/RefundRequest.py
@@ -10,7 +10,22 @@ from .ReturnInformation import ReturnInformation
 class RefundRequest:
     amountOfMoney: Optional[PositiveAmountOfMoney] = None
     references: Optional[PaymentReferences] = None
-    # TODO: Check if this works for the reserved word
-    return_: Optional[ReturnInformation] = field(
+    # "return" is a reserved keyword in Python, so we need to use a different name for the field:
+    return_info: Optional[ReturnInformation] = field(
         default=None, metadata={"name": "return"}
     )
+
+    # To adhere to the PAYONE API Schema, we need to use the name "return" externally.
+    # However, to avoid conflicts with Python's reserved keyword "return",
+    # we use "return_info" internally while mapping it to "return" for external interactions:
+    def __post_init__(self):
+        if self.return_info is not None:
+            setattr(self, "return", self.return_info)
+
+    def to_dict(self):
+        result = {
+            "amountOfMoney": self.amountOfMoney,
+            "references": self.references,
+            "return": self.return_info,
+        }
+        return result
