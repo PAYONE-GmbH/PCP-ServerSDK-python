f7486f0eab3a1f4c69812f7895d5a60da8c475d1
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2025-05-22 11:28:01 +0200

    docs: improve docstring for taxAmountPerUnit in OrderLineDetailsInput

--- pcp_serversdk_python/models/OrderLineDetailsInput.py
+++ pcp_serversdk_python/models/OrderLineDetailsInput.py
@@ -32,8 +32,10 @@ class OrderLineDetailsInput:
     @maximum 2147483647"""
 
     taxAmountPerUnit: Optional[bool] = None
-    """If this is set to `true`, `taxAmount` will be interpreted as the tax
-    amount per unit as opposed to the tax amount per line item."""
+    """This field indicates if the `taxAmount` is to be interpreted as the tax 
+    amount per unit rather than for the entire line item.
+    This field is included in the response only when `taxAmount` is set; otherwise, 
+    it will return as `null`."""
 
     productUrl: Optional[str] = None
     """URL of the product in the shop. Used for PAYONE Buy Now, Pay Later (BNPL)."""
