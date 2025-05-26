30c8fc53abd6a26683f659696058ffa266453e87
Blackfaded <r.heinen@nanogiants.de>
2025-05-26 09:50:50 +0200

    chore: update version to

--- scripts.sh
+++ scripts.sh
@@ -74,7 +74,7 @@ version() {
     npm run changelog
     git add CHANGELOG.md
     git tag -a v$NEW_VERSION -m "Version $NEW_VERSION"
-    git commit -m "Update version to $VERSION"
+    git commit -m "chore: update version to $VERSION"
     git push origin tag v$NEW_VERSION
     git push origin HEAD
     echo "Version complete."
