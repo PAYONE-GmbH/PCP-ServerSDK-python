85fd894b97ff5450e405e113eda5fb4129e099cd
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2025-07-10 16:14:39 +0200

    chore: update version to 1.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pcp_serversdk_python",
-    version="1.2.0",
+    version="1.3.0",
     author="PAYONE-GmbH",
     author_email="",
     description="",
