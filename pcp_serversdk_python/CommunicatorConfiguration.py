8996bdc33910ae4ef0a1193e56d3ec80c2589d37
Stefan Neidig <stefan@rpdev.net>
2025-09-02 11:14:28 +0200

    feat: add customization options for http client

--- pcp_serversdk_python/CommunicatorConfiguration.py
+++ pcp_serversdk_python/CommunicatorConfiguration.py
@@ -1,8 +1,20 @@
+from typing import Optional
+
+import httpx
+
+
 class CommunicatorConfiguration:
-    def __init__(self, api_key: str, api_secret: str, host: str):
+    def __init__(
+        self,
+        api_key: str,
+        api_secret: str,
+        host: str,
+        http_client: Optional[httpx.AsyncClient] = None
+    ):
         self.api_key = api_key
         self.api_secret = api_secret
         self.host = host
+        self.http_client = http_client
 
     def get_api_key(self) -> str:
         return self.api_key
@@ -12,3 +24,9 @@ class CommunicatorConfiguration:
 
     def get_host(self) -> str:
         return self.host
+
+    def get_http_client(self) -> Optional[httpx.AsyncClient]:
+        return self.http_client
+
+    def set_http_client(self, http_client: Optional[httpx.AsyncClient]) -> None:
+        self.http_client = http_client
