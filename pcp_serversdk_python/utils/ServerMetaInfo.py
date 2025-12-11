0dc27ccbda8addf1fc4a7e56b72baf6d71aeb6ec
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2025-12-11 09:45:53 +0100

    chore: update version to 1.5.0

--- pcp_serversdk_python/utils/ServerMetaInfo.py
+++ pcp_serversdk_python/utils/ServerMetaInfo.py
@@ -6,6 +6,6 @@ class ServerMetaInfo:
         self.platformIdentifier = (
             f"{platform.system()}, Python version is: {platform.python_version()}"
         )
-        self.sdkIdentifier = "PythonServerSDK/v1.4.0"  # Update version as needed
+        self.sdkIdentifier = "PythonServerSDK/v1.5.0"  # Update version as needed
         self.sdkCreator = "PAYONE GmbH"
         self.integrator = integrator
