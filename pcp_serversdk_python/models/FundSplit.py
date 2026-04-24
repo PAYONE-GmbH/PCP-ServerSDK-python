55bd1599658484c5d690e39556f309bc17a01c47
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2026-04-24 07:49:26 +0200

    feat: update API version to 1.50.0

--- pcp_serversdk_python/models/FundSplit.py
+++ pcp_serversdk_python/models/FundSplit.py
@@ -6,7 +6,9 @@ from .FundDistribution import FundDistribution
 
 @dataclass(kw_only=True)
 class FundSplit:
-    """Instructions for distributing funds in a marketplace context."""
+    """Instructions for distributing funds to multiple suppliers or partners in a
+    marketplace context. Only allowed for marketplace merchants or if feature to
+    ignore Marketplace fields is enabled in configuration."""
 
     id: Optional[str] = None
     """Unique identifier of the fund split. Read-only UUID."""
